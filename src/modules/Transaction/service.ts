8e25a5854f984fb8c1b2b7a26fbb1730486c7d09
nhatminh0509 <minhnhat@bacoor.co>
2023-09-11 19:51:14 +0700

    chore: fail:

--- src/modules/Transaction/service.ts
+++ src/modules/Transaction/service.ts
@@ -7,59 +7,67 @@ import NFTService from '../NFT/service'
 
 export default class TransactionService {
 	static async call(chainId: number, rawTx: string) {
-		const chains = await AppService.getChains()
-		const chain = await chains.find((i) => i.chainId === chainId)
-		if (!chain) {
-			throw Exception.BadRequest('Chain not found')
-		}
-		const caver = new Caver(chain.rpc[0])
-		caver.klay.accounts.wallet.add(AppConfig.callerKeys[0], '0xDF61031025A0f177314c10eB4bddF35B9E9bddd0')
-		const res = await caver.klay.sendTransaction({
-			senderRawTransaction: rawTx,
-			feePayer: '0xDF61031025A0f177314c10eB4bddF35B9E9bddd0',
-		})
+		try {
+			const chains = await AppService.getChains()
+			const chain = await chains.find((i) => i.chainId === chainId)
+			if (!chain) {
+				throw Exception.BadRequest('Chain not found')
+			}
+			const caver = new Caver(chain.rpc[0])
+			caver.klay.accounts.wallet.add(AppConfig.callerKeys[0], '0xDF61031025A0f177314c10eB4bddF35B9E9bddd0')
+			const res = await caver.klay.sendTransaction({
+				senderRawTransaction: rawTx,
+				feePayer: '0xDF61031025A0f177314c10eB4bddF35B9E9bddd0',
+			})
 
-		if (res?.logs) {
-			for (let i = 0; i < res.logs.length; i++) {
-				const log = res.logs[i]
-				const topic = log.topics?.[0]
-				if (topic === NFTCarTopics.BaseCollectionMinted) {
-					const decoded = ifaceCar.decodeEventLog('BaseCollectionMinted', log.data, log.topics)
-					const [recipient, type, tokenId] = decoded
-					console.log(recipient, type, tokenId, 'base')
-					await NFTService.handleMint({
-						chainId,
-						nftAddress: log.address.toLowerCase(),
-						nftId: `${tokenId}`,
-						to: `${recipient}`.toLowerCase(),
-						txHash: log.transactionHash,
-						type: `${type}`,
-					})
-				} else if (topic === NFTAccessoryTopics.AccessoryMinted) {
-					const decoded = ifaceAccessory.decodeEventLog('AccessoryMinted', log.data, log.topics)
-					const [recipient, type, tokenId] = decoded
-					console.log(recipient, type, tokenId, 'part')
-					await NFTService.handleMint({
-						chainId,
-						nftAddress: log.address.toLowerCase(),
-						nftId: `${tokenId}`,
-						to: `${recipient}`.toLowerCase(),
-						txHash: log.transactionHash,
-						type: `${type}`,
-					})
-				} else if (topic === RegistryTopics.AccountCreated) {
-					const decoded = ifaceRegistry.decodeEventLog('AccountCreated', log.data, log.topics)
-					const [account, implementation, cId, tokenContract, tokenId, salt] = decoded
-					console.log(`TBA Address `, { account, implementation, cId, tokenContract, tokenId, salt })
-					await NFTService.handleSaveTBA({
-						chainId: parseInt(`${cId}`),
-						nftAddress: `${tokenContract}`.toLowerCase(),
-						nftId: `${tokenId}`,
-						tbaAddress: `${account}`.toLowerCase(),
-					})
+			if (res?.logs) {
+				for (let i = 0; i < res.logs.length; i++) {
+					const log = res.logs[i]
+					const topic = log.topics?.[0]
+					if (topic === NFTCarTopics.BaseCollectionMinted) {
+						const decoded = ifaceCar.decodeEventLog('BaseCollectionMinted', log.data, log.topics)
+						const [recipient, type, tokenId] = decoded
+						console.log(recipient, type, tokenId, 'base')
+						await NFTService.handleMint({
+							chainId,
+							nftAddress: log.address.toLowerCase(),
+							nftId: `${tokenId}`,
+							to: `${recipient}`.toLowerCase(),
+							txHash: log.transactionHash,
+							type: `${type}`,
+						})
+					} else if (topic === NFTAccessoryTopics.AccessoryMinted) {
+						const decoded = ifaceAccessory.decodeEventLog('AccessoryMinted', log.data, log.topics)
+						const [recipient, type, tokenId] = decoded
+						console.log(recipient, type, tokenId, 'part')
+						await NFTService.handleMint({
+							chainId,
+							nftAddress: log.address.toLowerCase(),
+							nftId: `${tokenId}`,
+							to: `${recipient}`.toLowerCase(),
+							txHash: log.transactionHash,
+							type: `${type}`,
+						})
+					} else if (topic === RegistryTopics.AccountCreated) {
+						const decoded = ifaceRegistry.decodeEventLog('AccountCreated', log.data, log.topics)
+						const [account, implementation, cId, tokenContract, tokenId, salt] = decoded
+						console.log(`TBA Address `, { account, implementation, cId, tokenContract, tokenId, salt })
+						await NFTService.handleSaveTBA({
+							chainId: parseInt(`${cId}`),
+							nftAddress: `${tokenContract}`.toLowerCase(),
+							nftId: `${tokenId}`,
+							tbaAddress: `${account}`.toLowerCase(),
+						})
+					}
 				}
 			}
+			return res
+		} catch (error) {
+			console.log(error)
+			return {
+				fail: true,
+				error: error,
+			}
 		}
-		return res
 	}
 }
